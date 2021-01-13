f6da9e657298011ef66b20a561675c7ef4a656ad
shawnstom <49721452+shawnstom@users.noreply.github.com>
2021-01-13 10:53:26 -0500

    Commit Files

---
+++ page-templates/coming-soon.php
@@ -0,0 +1,83 @@
+<?php
+   /*
+    Template Name: coming-soon
+    */
+    if ( ! defined( 'ABSPATH' ) ) {
+      exit; // Exit if accessed directly.
+    }
+    ?>
+<?php get_header(); ?>
+
+<section id="page-header" class="coming-soon">
+    <img src="/wp-content/themes/ember/img/ember-logo.svg" max-width="400">
+    <div class="flame">
+        <?php echo file_get_contents('wp-content/themes/ember/img/flame.svg'); ?>
+        <div>
+            <h1 class="special">Ignite</h1>
+            <h2>Your Health.</h2>
+        </div>
+        <p class="special">Better <span><strong>Care</strong></span>. Better <span><strong>Health</strong></span>. Better <span><strong>You</strong></span>.</p>
+        <p class="opening">Opening <span>Spring 2021</span> | Greenville, SC</p>
+    </div>
+    <div class="main">
+        <p>
+            <strong>
+                <span>Introducing Ember.</span><br>
+                &nbsp;A direct primary care practice focused<br>
+                &nbsp;on a more personal approach to medicine.
+            </strong>
+        </p>
+        <p>We provide a more comprehensive pathway to personal well-being,<br>
+            &nbsp;combining <span>lifestyle</span>, <span>nutrition</span>, <span>health training</span>, and <span>direct access</span> to your<br> 
+            &nbsp;medical provider 24/7 for a fixed monthly cost without a limit or co-pays.
+        </p>
+        <div class="signup">
+            <p>Sign-up to receive more information on our opening, upcoming events,<br>
+            &nbsp;and to learn how Ember plans to change the way you're treated.</p>
+            <?php //echo FrmFormsController::get_form_shortcode( array( 'id' => 3, 'title' => false, 'description' => false ) ); ?>
+            <button id="popup-btn" role="button">Sign Up</button>
+        </div>
+    </div>
+    <!--div class="social">
+        <a href="" target="_blank"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+        <a href="" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+    </div-->
+    <div class="popup-wrapper">
+        <div class="popup">
+            <div class="close">X</div>
+            <?php echo file_get_contents('wp-content/themes/ember/img/flame.svg'); ?>
+            <h2>Learn about a more personal approach to</h2>
+            <h1><span><em>Modern</em></span> Medicine</h1>
+            <p>We look forward to caring for you and your family’s needs.</p>
+            <?php echo FrmFormsController::get_form_shortcode( array( 'id' => 8, 'title' => false, 'description' => false ) ); ?>
+            <?php echo file_get_contents('wp-content/themes/ember/img/flame-03.svg'); ?>
+        </div>
+    </div>
+</section>
+
+<script async defer>
+    jQuery(document).ready(function($) {
+        $('#popup-btn').click(function() {
+            if ( $('.popup-wrapper').hasClass('show') ) {
+                $('.popup-wrapper').css("opacity", 0);
+                setTimeout(function() {
+                    $('.popup-wrapper').removeClass("show");
+                }, 600);
+            } else {
+                $('.popup-wrapper').addClass("show");
+                setTimeout(function() {
+                    $('.popup-wrapper').css("opacity", 1);
+                }, 50);
+            }
+        });
+
+        $('.close').click(function() {
+            $('.popup-wrapper').css("opacity", 0);
+            setTimeout(function() {
+                $('.popup-wrapper').removeClass("show");
+            }, 600);
+        })
+    });
+</script>
+
+<?php get_footer(); ?>
---
+++ page-templates/home.php
@@ -0,0 +1,58 @@
+<?php
+   /*
+    Template Name: Home
+    */
+    if ( ! defined( 'ABSPATH' ) ) {
+      exit; // Exit if accessed directly.
+    }
+    ?>
+<?php get_header(); ?>
+
+<section id="page-header" class="home">
+    <div class="home-header">
+        <div class="home-inner">
+            <div class="title-block">
+                <h1 class="text-center">You Are <br><span>&nbsp;Extraordinary</span></h1>
+                <h2></h2>
+            </div>
+
+        </div>
+    </div>
+    <div class="header-bottom-bar">
+        <div class="social"></div>
+        <div class="discover"></div>
+    </div>
+</section>
+<section id="home-intro pl-5">
+    <div class="container">
+        <div class="row home-intro-1">
+            <div class="col">
+                <div class="subtitle pl-5"></div>
+                <div class="divider"></div>
+            </div>
+        </div>
+        <div class="row home-intro-2 pl-5">
+            <div class="col-lg-6">
+
+            </div>
+            <div class="img-float">
+                <img src="" alt="">
+            </div>
+        </div>
+        <div class="row home-intro-3 pl-5">
+            <div class="col-md-5">
+                <div><img src="" alt=""></div>
+            </div>
+            <div class="col-md-5 offset-md-1">
+                <h3 class="special"></h3>
+                <div class="divider"></div>
+                <p class="mt-5"></p>
+                <div class="link">
+                    <a href=""></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- <?php get_template_part('/template-parts/cta') ?> -->
+<?php get_footer(); ?>
