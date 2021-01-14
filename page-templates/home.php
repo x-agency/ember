9dbdc002d9029386e7a769ea8748ac637eec29c5
shawnstom <49721452+shawnstom@users.noreply.github.com>
2021-01-14 02:48:37 -0500

    Shawn Update

--- page-templates/home.php
+++ page-templates/home.php
@@ -43,8 +43,215 @@
             </div>
             <div class="col-12">
                 <h3>A <span>member-based, direct primary care practice</span> focused on a more comprehensive, personal
-                    approach to
-                    medicine.</h3>
+                    approach to medicine.</h3>
+            </div>
+        </div>
+        <div class="row intro-grid">
+            <div class="col-md-4 intro-grid-item">
+                <div class="grid-img"><img src="/wp-content/themes/ember/img/intro-img-1.jpg" alt=""></div>
+                <div class="grid-title"><span>Member-first</span> Care.</div>
+                <div class="grid-number">
+                    <div class="number">1</div>
+                </div>
+            </div>
+            <div class="col-md-4 intro-grid-item">
+                <div class="grid-img"><img src="/wp-content/themes/ember/img/intro-img-2.jpg" alt=""></div>
+                <div class="grid-title"><span>Whole-person</span> Approach.</div>
+                <div class="grid-number">
+                    <div class="number">2</div>
+                </div>
+            </div>
+            <div class="col-md-4 intro-grid-item">
+                <div class="grid-img"><img src="/wp-content/themes/ember/img/intro-img-3.jpg" alt=""></div>
+                <div class="grid-title"><span>Tailored</span> Lifestyle Plan.</div>
+                <div class="grid-number">
+                    <div class="number">3</div>
+                </div>
+            </div>
+        </div>
+        <div class="row intro-text">
+            <div class="col-12 title">
+                <h1>Getting to the <span>heart</span> of good health.</h1>
+                <div class="heart-icon"><img src="/wp-content/themes/ember/img/svg/heart-icon.svg" alt=""></div>
+            </div>
+            <div class="col-md-6 block-1">
+                <p>As a pioneer in personalized medicine, Ember brings a fresh new perspective to
+                    <span>relationship-based healthcare</span>. To help guide you to your best health, we start by
+                    getting to know you andunderstanding your full lifestyle spectrum in order to treat the whole person
+                    and not just symptoms or disease.
+                    Plus, our transformative approach in offering <span>member-based direct primary care</span> to our
+                    clients is a
+                    revolutionary idea that should be common place.
+                </p>
+            </div>
+            <div class="col-md-6">
+                <p>Our clear, up-front membership cost with zero copays creates a <span>simple and stress-free
+                        healthcare experience</span>. With Ember, our focus is clear—to empower your health by removing
+                    the barriers to yourm care provider. Trade the typical 15-minute annual visits found with
+                    traditional doctors for longer, more meaningful time designed to meet your health needs and goals.
+                    This is what true medical care
+                    was designed to be—<span>complete access and personal attention when it matters most</span>.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="meet-brian">
+    <div class="container">
+        <div class="row meet-wrapper">
+            <div class="meet-inner">
+                <h1>Meet <span>Brian</span>.</h1>
+                <p>Dr. Brian Blank, board certified in family medicine, is the founder of Ember Modern Medicine. Having
+                    completed medical school at the University of North Carolina at Chapel Hill, he went on to study
+                    family medicine at Duke University Hospital. Prior to medical training, Dr. Blank worked in cable
+                    news. He got a bachelor of journalism at the University of Missouri. He was a researcher, producer
+                    and freelance reporter at CNN before producing at the Fox Business Network. He likes to joke that
+                    because he has worked at both CNN and Fox, he is by nature fair and balanced. In his free time, Dr.
+                    Blank enjoys competing in endurance events. He has completed more than a dozen marathons, a 50-mile
+                    ultramarathon and one Ironman Triathlon. Lately, he has been attending CrossFit as well as running
+                    on the Swamp Rabbit Trail with his wife and two daughters.</p>
+                <div class="brian-img"><img src="/wp-content/themes/ember/img/brian-img.jpg" alt=""></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="quote-block">
+                “Our focus from day one is to provide a more meaningful experience in your health journey to ignite a
+                passion for the destination.” <span>—Dr. Blank<span>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="about-member">
+    <div class="container">
+        <div class="row member-benefits member">
+            <div class="benefits-left">
+                <img class="img-fluid" src="/wp-content/themes/ember/img/home-img-1.jpg" alt="">
+                <div class="about-flame"><img src="/wp-content/themes/ember/img/svg/flame-icon.svg" alt=""></div>
+                <div class="about-title">
+                    <h1><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;People-<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Centered</span>.
+                        <br>Transformative
+                        <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Health.
+                    </h1>
+                </div>
+                <div class="about-btn"><a href="">I'm Interested</a></div>
+            </div>
+            <div class="member-block">
+                <div class="member-title">Member Privileges:</div>
+                <div class="member-content">
+                    <div class="icon"><img src="/wp-content/themes/ember/img/member-icon-1.png" alt=""></div>
+                    <div class="title">Unlimited Access</div>
+                    <div class="text">Get truly unlimited access to an Ember doctor at any time</div>
+                </div>
+                <div class="member-content">
+                    <div class="icon"><img src="/wp-content/themes/ember/img/member-icon-2.png" alt=""></div>
+                    <div class="title">Office Visits (same-day or next)</div>
+                    <div class="text">Schedule a visit and be guaranteed same-day or next day appointments</div>
+                </div>
+                <div class="member-content">
+                    <div class="icon"><img src="/wp-content/themes/ember/img/member-icon-3.png" alt=""></div>
+                    <div class="title">Clear, Up-front Cost</div>
+                    <div class="text">No hassle. No copays. Our members pay a flat monthly fee for access to all of
+                        Ember’s services.</div>
+                </div>
+                <div class="special">*Home visits upon request; additional charges may apply</div>
+            </div>
+        </div>
+        <div class="row member-services member">
+            <div class="member-block">
+                <div class="member-title">Member Privileges:</div>
+                <div class="services-list">
+                    <ul class="list-unstyled">
+                        <li>In-depth annual physicals</li>
+                        <li>Sports and camp physicals</li>
+                        <li>Urgent Care</li>
+                        <li>Primary Care</li>
+                        <li>Chronic disease management</li>
+                        <li>Curbside COVID tests</li>
+                        <li>Body composition analysis</li>
+                        <li>In-office procedures</li>
+                        <li>Coordination with specialists</li>
+                        <li>In-office labwork free</li>
+                        <li>Prescription program</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="services-right">
+                <img class="img-fluid" src="/wp-content/themes/ember/img/home-img-2.jpg" alt="">
+                <div class="services-title">
+                    <h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We <br><span>&nbsp;&nbsp;Invite</span> <br>
+                        &nbsp;&nbsp;&nbsp;&nbsp;You To
+                        <br>&nbsp;&nbsp;Explore
+                        <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;the
+                        <br>Difference.
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="pricing">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1 class="mb-1">At <span style="color: #133738">Ember</span>, we treat people <span>well</span>.</h1>
+                <h3>And our membership pricing is no different.</h3>
+            </div>
+        </div>
+        <div class="row pricing-grid">
+            <div class="price-item">
+                <div class="price-title"><span>MONTHLY</span> Membership</div>
+                <div class="price-number">$100<span>/mo</span></div>
+                <div class="price-billed">Billed in twelve monthly installments</div>
+                <div class="price-text">
+                    <p><span>Children 0-17:</span> $20/month with at least one parent membership</p>
+                    <p>*Family discounts available</p>
+                </div>
+                <div class="price-btn"><button href="">Early Sign Up</button></div>
+            </div>
+            <div class="price-item">
+                <div class="price-title"><span>ANNUAL</span> Membership</div>
+                <div class="price-number">$80<span>/mo</span></div>
+                <div class="price-billed">Billed in twelve monthly installments</div>
+                <div class="price-text">
+                    <p><span>Children 0-17:</span> $20/month with at least one parent membership</p>
+                    <p>*Family discounts available</p>
+                </div>
+                <div class="price-btn"><button href="">Early Sign Up</button></div>
+            </div>
+            <div class="price-item">
+                <div class="price-title"><span>MONTHLY</span> Membership</div>
+                <div class="price-number">Contact Us</div>
+                <div class="price-text">
+                    <p>Ember offers comprehensive packages for business looking to provide employees with a better
+                        alternative to traditional healthcare.</p>
+                    <p>Reach out to us to learn more and to get pricing.</p>
+                </div>
+                <div class="price-btn"><button href="">Early Sign Up</button></div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="membership-advantages">
+    <div class="container">
+        <div class="row advantages">
+            <h2>Membership Has Its Advantages</h1>
+                <h3>Curious about how our membership pricing compares to the average cost of current medical care?
+                    <span>Let
+                        us
+                        show you.</span>
+                </h3>
+        </div>
+    </div>
+</section>
+<section id="cta">
+    <div class="container">
+        <div class="cta-block">
+            <div class="cta-text">
+                <h1>Interested to <br><span>&nbsp;&nbsp;&nbsp;&nbsp;learn more</span>?</h1>
+                <p>We’re not quite ready to open the doors just yet, but we’re getting everything ready for you and will
+                    be launching Ember Modern Medicine in early 2021.</p>
+                <p>If you’d like to be one of the first to get early sign-up pricing and want to receive updates as we
+                    get closer to launch, head over to the contact page and share your information with us.</p>
+                <button>Keep Me Updated</button>
             </div>
         </div>
     </div>
